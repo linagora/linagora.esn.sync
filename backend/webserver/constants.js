caae331c0de42d8293d9891a97d45d1b2ee46bf0
Christophe Hamerling <chamerling@linagora.com>
2018-10-18 14:47:57 +0200

    linagora/lgs/openpaas/esn#2529 Add downloads folder

--- backend/webserver/constants.js
+++ backend/webserver/constants.js
@@ -1,3 +1,4 @@
 'use strict';
 
 module.exports.FRONTEND_PATH = require('path').normalize(__dirname + '/../../frontend');
+module.exports.DOWNLOAD_PATH = require('path').normalize(__dirname + '/../../downloads');
